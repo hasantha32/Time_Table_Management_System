6ed4436160d2072933fe8ea0d725a0972e8c53d9
hasantha32 <hasanthamadushan32@gmail.com>
2021-05-20 15:14:59 -0800

    updated

--- Time_Table_Management/src/Session_and_TimeAllocation/SessionsANDnotAvailableTimeAllocationConsecutive.java
+++ Time_Table_Management/src/Session_and_TimeAllocation/SessionsANDnotAvailableTimeAllocationConsecutive.java
@@ -17,6 +17,7 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JSeparator;
@@ -49,8 +50,24 @@ import javax.swing.table.JTableHeader;
 import javax.swing.table.TableColumnModel;
 
 import DBC.DBConnection;
+import Home.StartUp;
+import Leactures_and_Subjects.Add_Lecturer;
+import Leactures_and_Subjects.Add_Subjects;
+import Leactures_and_Subjects.Manage_Lecturer;
+import Leactures_and_Subjects.Manage_Subjects;
+import Location_and_Statistics.Add_Locations;
+import Location_and_Statistics.Add_Locations_Sessions;
+import Location_and_Statistics.Manage_Locations;
+import Location_and_Statistics.Visualizing_static;
+import Students_and_Tags.Add_Student_Groups;
+import Students_and_Tags.Add_Tag;
+import Students_and_Tags.Manage_Student_Groups;
+import Students_and_Tags.Manage_Tags;
 import Timetable.Lecturer;
-
+import Timetable.Location;
+import Timetable.Student;
+import Working_Days_and_Hours.Add_Workingdays_Hours;
+import Working_Days_and_Hours.Manage_Workingdays_Hours;
 import net.proteanit.sql.DbUtils;
 
 import javax.swing.border.LineBorder;
@@ -58,12 +75,13 @@ import java.awt.Panel;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 public class SessionsANDnotAvailableTimeAllocationConsecutive {
 
 	
 	
-	
+	private JFrame frame;
 	private JFrame frmAddStudentGroup;
 	private JTable table;
 	private JComboBox comboBox_s1;
@@ -508,6 +526,473 @@ public class SessionsANDnotAvailableTimeAllocationConsecutive {
 			menuBar.setBackground(Color.BLACK);
 			menuBar.setBounds(-1, 0, 1344, 37);
 			frmAddStudentGroup.getContentPane().add(menuBar);
+			
+			JMenu mnNewMenu = new JMenu("Category");
+			mnNewMenu.setFont(new Font("Segoe UI", Font.BOLD, 15));
+			mnNewMenu.setBackground(Color.BLACK);
+			mnNewMenu.setForeground(Color.YELLOW);
+			menuBar.add(mnNewMenu);
+			
+			JMenu mnNewMenu_1 = new JMenu("Lectures & Subjects");
+			mnNewMenu_1.setBackground(new Color(128, 128, 128));
+			mnNewMenu.add(mnNewMenu_1);
+			
+			JMenuItem mntmNewMenuItem = new JMenuItem("Add Lecture");
+			mntmNewMenuItem.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					
+					Add_Lecturer add_Lecturer=new Add_Lecturer();
+					Add_Lecturer.main(null);
+					frame.setVisible(false);
+					
+				}
+				
+			});
+			mnNewMenu_1.add(mntmNewMenuItem);
+			
+			JMenuItem mntmNewMenuItem_1 = new JMenuItem("Add Subjects");
+			mntmNewMenuItem_1.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					
+					Add_Subjects add_Subjects=new Add_Subjects();
+					Add_Subjects.main(null);
+					frame.setVisible(false);
+					
+				}
+				
+			});
+			mnNewMenu_1.add(mntmNewMenuItem_1);
+			
+			JMenuItem mntmNewMenuItem_2 = new JMenuItem("Manage Lecturers");
+			mntmNewMenuItem_2.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					
+					Manage_Lecturer manage_Lecturer=new Manage_Lecturer();
+					Manage_Lecturer.main(null);
+					frame.setVisible(false);
+					
+				}
+				
+			});
+			mnNewMenu_1.add(mntmNewMenuItem_2);
+			
+			JMenuItem mntmNewMenuItem_3 = new JMenuItem("Manage Subjects");
+			mntmNewMenuItem_3.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					
+					Manage_Subjects manage_Subjects=new Manage_Subjects();
+					Manage_Subjects.main(null);
+					frame.setVisible(false);
+					
+				}
+				
+			});
+			mnNewMenu_1.add(mntmNewMenuItem_3);
+			
+			JMenuBar menuBar_1 = new JMenuBar();
+			mnNewMenu_1.add(menuBar_1);
+			
+			JMenu mnNewMenu_2 = new JMenu("Session & Time Allocation");
+			mnNewMenu.add(mnNewMenu_2);
+			
+			JMenuItem mntmNewMenuItem_5 = new JMenuItem("Add Session");
+			mntmNewMenuItem_5.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					
+					Add_Session add_Session=new Add_Session();
+					Add_Session.main(null);
+					frame.setVisible(false);
+					
+				}
+				
+			});
+			mnNewMenu_2.add(mntmNewMenuItem_5);
+			
+			JMenuItem mntmNewMenuItem_4 = new JMenuItem("Manage Not Available Times");
+			mntmNewMenuItem_4.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					
+					Manage_Not_Available_Times manage_Not_Available_Times=new Manage_Not_Available_Times();
+					Manage_Not_Available_Times.main(null);
+					frame.setVisible(false);
+					
+				}
+				
+			});
+			mnNewMenu_2.add(mntmNewMenuItem_4);
+			
+			JMenuItem mntmNewMenuItem_6 = new JMenuItem("Consecutive Session rooms");
+			mntmNewMenuItem_6.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					
+					ConsecutiveSessionRooms manage_Session_Rooms=new ConsecutiveSessionRooms();
+					ConsecutiveSessionRooms.main(null);
+					frame.setVisible(false);
+					
+				}
+				
+			});
+			
+			mnNewMenu_2.add(mntmNewMenuItem_6);
+			
+			JMenuItem mntmNewMenuItem_7 = new JMenuItem("Manage Sessions");
+			mntmNewMenuItem_7.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					
+					Manage_Sessions manage_Sessions=new Manage_Sessions();
+					Manage_Sessions.main(null);
+					frame.setVisible(false);
+					
+				}
+				
+			});
+			
+			
+			mnNewMenu_2.add(mntmNewMenuItem_7);
+			
+			///
+			
+			
+			JMenuItem mntmNewMenuItem_21 = new JMenuItem("Session Rooms");
+			mntmNewMenuItem_21.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					
+					SessionsRooms manage_Session_Rooms=new SessionsRooms();
+					SessionsRooms.main(null);
+					frame.setVisible(false);
+					
+				}
+				
+			});
+			
+			mnNewMenu_2.add(mntmNewMenuItem_21);
+			///
+			
+			
+			
+			
+			JMenuItem mntmNewMenuItem_8 = new JMenuItem("Not Available Time");
+			mntmNewMenuItem_8.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					
+					Not_Available_Time not_Available_Time=new Not_Available_Time();
+					Not_Available_Time.main(null);
+					frame.setVisible(false);
+					
+				}
+				
+			});
+			
+			
+			mnNewMenu_2.add(mntmNewMenuItem_8);
+			
+			JMenuItem mntmNewMenuItem_9 = new JMenuItem("Sessions & Not Available Time Allocation Consecutive");
+			mntmNewMenuItem_9.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					
+					SessionsANDnotAvailableTimeAllocationConsecutive sessionsANDnotAvailableTimeAllocationConsecutive=new SessionsANDnotAvailableTimeAllocationConsecutive();
+					SessionsANDnotAvailableTimeAllocationConsecutive.main(null);
+					frame.setVisible(false);
+					
+				}
+				
+			});
+			mnNewMenu_2.add(mntmNewMenuItem_9);
+			
+			
+			JMenuItem mntmNewMenuItem_10 = new JMenuItem("Sessions & Not Available Time Allocation Non Overlaping");
+			mntmNewMenuItem_10.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					
+					SessionsANDnotAvailableTimeAllocationNoneOverlaping sessionsANDnotAvailableTimeAllocationNoneOverlaping=new SessionsANDnotAvailableTimeAllocationNoneOverlaping();
+					SessionsANDnotAvailableTimeAllocationNoneOverlaping.main(null);
+					frame.setVisible(false);
+					
+				}
+				
+			});
+			mnNewMenu_2.add(mntmNewMenuItem_10);
+			
+			
+			JMenuItem mntmNewMenuItem_31 = new JMenuItem("Sessions & Not Available Time Allocation Parallel");
+			mntmNewMenuItem_31.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					
+					SessionsANDnotAvailableTimeAllocationParallel sessionsANDnotAvailableTimeAllocationParallel=new SessionsANDnotAvailableTimeAllocationParallel();
+					SessionsANDnotAvailableTimeAllocationParallel.main(null);
+					frame.setVisible(false);
+					
+				}
+				
+			});
+			mnNewMenu_2.add(mntmNewMenuItem_31);
+
+
+			
+			JMenu mnNewMenu_3 = new JMenu("Working Days & Hours ");
+			mnNewMenu.add(mnNewMenu_3);
+			
+			JMenuItem mntmNewMenuItem_11 = new JMenuItem("Add Workingdays Hours");
+			mntmNewMenuItem_11.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					
+					Add_Workingdays_Hours add_Workingdays_Hours=new Add_Workingdays_Hours();
+					Add_Workingdays_Hours.main(null);
+					frame.setVisible(false);
+					
+				}
+				
+			});
+			mnNewMenu_3.add(mntmNewMenuItem_11);
+			
+			
+			
+			JMenuItem mntmNewMenuItem_65 = new JMenuItem("Manage Workingdays Hours");
+			mntmNewMenuItem_65.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					
+					Manage_Workingdays_Hours add_Workingdays_Hours=new Manage_Workingdays_Hours();
+					Manage_Workingdays_Hours.main(null);
+					frame.setVisible(false);
+					
+				}
+				
+			});
+			mnNewMenu_3.add(mntmNewMenuItem_65);
+			
+			
+			JMenu mnNewMenu_4 = new JMenu("Student & Tags");
+			mnNewMenu.add(mnNewMenu_4);
+			
+			JMenuItem mntmNewMenuItem_14 = new JMenuItem("Add Student Groups");
+			
+			mntmNewMenuItem_14.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					
+					Add_Student_Groups addstudentgroups=new Add_Student_Groups();
+					Add_Student_Groups.main(null);
+					frame.setVisible(false);
+					
+				}
+				
+			});
+			
+			
+			mnNewMenu_4.add(mntmNewMenuItem_14);
+			
+			JMenuItem mntmNewMenuItem_15 = new JMenuItem("Add Tag");
+			mntmNewMenuItem_15.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					
+					Add_Tag addtags=new Add_Tag();
+					Add_Tag.main(null);
+					frame.setVisible(false);
+					
+				}
+				
+			});
+			
+			mnNewMenu_4.add(mntmNewMenuItem_15);
+			
+			JMenuItem mntmNewMenuItem_16 = new JMenuItem("Manage Student Groups");
+			
+			
+			mntmNewMenuItem_16.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					
+					Manage_Student_Groups managestudentgroups=new Manage_Student_Groups();
+					Manage_Student_Groups.main(null);
+					frame.setVisible(false);
+					
+				}
+				
+			});
+			
+			mnNewMenu_4.add(mntmNewMenuItem_16);
+			
+			JMenuItem mntmNewMenuItem_17 = new JMenuItem("Manage Tags");
+			
+			
+			mntmNewMenuItem_17.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					
+					Manage_Tags managetags=new Manage_Tags();
+					Manage_Tags.main(null);
+					frame.setVisible(false);
+					
+				}
+				
+			});
+			mnNewMenu_4.add(mntmNewMenuItem_17);
+			
+			JMenu mnNewMenu_5 = new JMenu("Location & Statistics");
+			mnNewMenu.add(mnNewMenu_5);
+			
+			JMenuItem mntmNewMenuItem_18 = new JMenuItem("Add Locations Sessions");
+			
+			mntmNewMenuItem_18.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					
+					Add_Locations_Sessions addlocationssessions=new Add_Locations_Sessions();
+					Add_Locations_Sessions.main(null);
+					frame.setVisible(false);
+					
+				}
+				
+			});
+			
+			
+			mnNewMenu_5.add(mntmNewMenuItem_18);
+			
+			JMenuItem mntmNewMenuItem_19 = new JMenuItem("Add Locations");
+			
+			
+			
+			mntmNewMenuItem_19.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					
+					Add_Locations addlocations=new Add_Locations();
+					Add_Locations.main(null);
+					frame.setVisible(false);
+					
+				}
+				
+			});
+			mnNewMenu_5.add(mntmNewMenuItem_19);
+			
+			JMenuItem mntmNewMenuItem_30 = new JMenuItem("Manage Locations");
+			
+			
+			
+			mntmNewMenuItem_30.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					
+					Manage_Locations managelocations=new Manage_Locations();
+					Manage_Locations.main(null);
+					frame.setVisible(false);
+					
+				}
+				
+			});
+			
+			mnNewMenu_5.add(mntmNewMenuItem_30);
+			
+			
+		JMenuItem mntmNewMenuItem_34 = new JMenuItem("Visualizing Static");
+			
+			
+			
+			mntmNewMenuItem_34.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					
+					Visualizing_static visualizing_static=new Visualizing_static();
+					Visualizing_static.main(null);
+					frame.setVisible(false);
+					
+				}
+				
+			});
+			
+			mnNewMenu_5.add(mntmNewMenuItem_34);
+			
+			
+			
+			JMenu mnNewMenu_33 = new JMenu("Generate Time Tables");
+			mnNewMenu.add(mnNewMenu_33);
+			
+			JMenuItem mntmNewMenuItem_111 = new JMenuItem("Lecturer");
+			mntmNewMenuItem_111.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					
+					Lecturer lec=new Lecturer();
+					Lecturer.main(null);
+					frame.setVisible(false);
+					
+				}
+				
+			});
+			mnNewMenu_33.add(mntmNewMenuItem_111);
+			
+			
+			
+			JMenuItem mntmNewMenuItem_655 = new JMenuItem("Student");
+			mntmNewMenuItem_655.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					
+					Student student=new Student();
+					Student.main(null);
+					frame.setVisible(false);
+					
+				}
+				
+			});
+			mnNewMenu_33.add(mntmNewMenuItem_655);
+
+			
+			JMenuItem mntmNewMenuItem_432 = new JMenuItem("Location");
+			mntmNewMenuItem_432.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					
+					Location location=new Location();
+					Location.main(null);
+					frame.setVisible(false);
+					
+				}
+				
+			});
+			mnNewMenu_33.add(mntmNewMenuItem_432);
+			
+			
+			
+			
+			
+			////////////////////////////////////////////////////////////
+			
+			
+			
+			
+			
+			
+			
+			
+			JMenu mnNewMenu_7 = new JMenu("About");
+			
+			
+
+
+			mnNewMenu_7.setFont(new Font("Segoe UI", Font.BOLD, 15));
+			mnNewMenu_7.setBackground(Color.BLACK);
+			mnNewMenu_7.setForeground(Color.WHITE);
+			menuBar.add(mnNewMenu_7);
+			
+			JMenuItem mntmNewMenuItem_22 = new JMenuItem("About");
+			
+			
+			
+			
+			mntmNewMenuItem_22.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					
+					StartUp startup=new StartUp();
+					StartUp.main(null);
+					frame.setVisible(false);
+					
+				}
+				
+			});
+			
+			mnNewMenu_7.add(mntmNewMenuItem_22);
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			//////////////////////////
 			
 			JPanel panel_1_1 = new JPanel();
 			panel_1_1.setBackground(Color.GRAY);
