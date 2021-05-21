03a373e09aefed17037f690c2f27c086a01f7f29
hasantha32 <hasanthamadushan32@gmail.com>
2021-05-20 21:44:56 -0800

    commited manage subject page

--- Time_Table_Management/src/Leactures_and_Subjects/Manage_Subjects.java
+++ Time_Table_Management/src/Leactures_and_Subjects/Manage_Subjects.java
@@ -139,7 +139,7 @@ public class Manage_Subjects {
 		panel_3.add(lblNewLabel_1);
 		
 		comboBox = new JComboBox();
-		comboBox.setModel(new DefaultComboBoxModel(new String[] {"2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021"}));
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"1", "2", "3", "4"}));
 		comboBox.setFont(new Font("Times New Roman", Font.BOLD, 16));
 		comboBox.setBackground(Color.LIGHT_GRAY);
 		comboBox.setBounds(211, 287, 191, 33);
