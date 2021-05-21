abdd9eca450830ac05f724916949e63201761d8e
hasantha32 <hasanthamadushan32@gmail.com>
2021-05-20 21:55:05 -0800

    updated

--- Time_Table_Management/src/Leactures_and_Subjects/Add_Subjects.java
+++ Time_Table_Management/src/Leactures_and_Subjects/Add_Subjects.java
@@ -125,7 +125,7 @@ public class Add_Subjects {
 		JComboBox comboBox = new JComboBox();
 		comboBox.setFont(new Font("Times New Roman", Font.BOLD, 16));
 		comboBox.setBackground(Color.LIGHT_GRAY);
-		comboBox.setModel(new DefaultComboBoxModel(new String[] {"2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021"}));
+		comboBox.setModel(new DefaultComboBoxModel(new String[] { "1", "2", "3", "4"}));
 		comboBox.setBounds(140, 53, 211, 33);
 		panel_3.add(comboBox);
 		
